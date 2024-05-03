1c5afc9ccdc680bf5924a22f16e2c7cf3509af57
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-05-03 15:23:11 -0700

    Added Logo to Footer

--- project-xotics/src/footer.js
+++ project-xotics/src/footer.js
@@ -1,17 +1,22 @@
 import './footer.css';
 import instagram from './assets/instagram.svg';
-
+import logo from './assets/logo-vector.svg'
 function Footer() {
   return (
 
     <footer>
       <div id="footer-header">
+      
+      <div className='logo-img'>
+        <img src={logo} alt="logo of Xotic Dip"/>
+        </div>
         <h2>Stay Exotic</h2>
         <h4>Be the first to know about new collections and exclusive offers.</h4>
       </div>
       <div className='icon-instagram'>
         <a href='https://www.instagram.com/xotic_dip/'><img src={instagram} alt='Instagram Logo'/></a>
       </div>
+      
       <p id="copyright">
         © 2024 Xotic Dip Powered by <a href='https://rafaelcxrrllx.github.io/trinitys.world/'>Trinity</a>
       </p>
